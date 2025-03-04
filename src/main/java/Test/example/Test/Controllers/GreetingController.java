9bbf746277cc185b8bce330b9854cb7987771cc1
Akshat_Sharma-15 <akshatsharma152002@gmail.com>
2025-03-04 21:58:24 +0530

    UC-2

--- src/main/java/Test/example/Test/Controllers/GreetingController.java
+++ src/main/java/Test/example/Test/Controllers/GreetingController.java
@@ -2,6 +2,7 @@ package Test.example.Test.Controllers;
 
 import Test.example.Test.dto.MessageDTO;
 import Test.example.Test.Services.GreetingService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,5 +22,15 @@ public class GreetingController {
     @PostMapping("/post")
     public String postGreetings(@RequestBody MessageDTO message){
         return "{\""+message.getMessage()+": \"Hello from POST Request!\"}";
+    }
+
+    @PutMapping("/put/{message}")
+    public String putGreetings(@PathVariable String message){
+        return "{\""+message+": \"Hello from PUT Request!\"}";
+    }
+
+    @GetMapping("/service")
+    public String serviceGreetings(){
+        return greetingService.getGreetings();
     }
 }
